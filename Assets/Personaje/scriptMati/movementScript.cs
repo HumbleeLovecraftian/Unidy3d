943324c5ad83caa1e884d31895d15a56f5e620e4
Galarza Matías <matiasgg1821@gmail.com>
2023-07-08 20:50:59 -0300

    Changes

--- Assets/Personaje/scriptMati/movementScript.cs
+++ Assets/Personaje/scriptMati/movementScript.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class mov : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    /* aaaa */
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
